dc7299f066a8cbf5d62317f7f74106b56da3adaf
luke <luke@luke-ThinkPad-E525.(none)>
2013-06-06 20:47:41 +0200

    2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Shop::Application.routes.draw do
+  root :to => "home#index"
+
+  get "home/index"
+
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

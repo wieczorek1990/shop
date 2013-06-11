1fe87b4df20d1dc594554e235a0dbfe09153d150
wieczorek1990 <wieczorek1990@gmail.com>
2013-06-11 19:57:32 +0200

    pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,8 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    #@products = Product.all
+    @products = Product.paginate(:page => params[:page], :per_page => 3)
 
     respond_to do |format|
       format.html # index.html.erb
